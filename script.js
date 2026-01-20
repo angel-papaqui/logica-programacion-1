df64c30b5336732476e3a05b70c731486deaa6c1
Angel Papaqui <angeldavidpapaqui@gmail.com>
2026-01-19 23:59:53 -0600

    feat: cambios en estilo para que se imprima el resultado en la página y no en la consola

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-let userNumber1 = Number(prompt("Ingresa un numero:"));
-let userNumber2 = Number(prompt("Ingresa otro numero:"));
-let userNumber3 = Number(prompt("Ingresa otro numero:"));
-let userNumbers = [userNumber1, userNumber2, userNumber3];
+function ordenarNumeros() {
+    const userNumber1 = Number(document.getElementById("num-1").value);
+    const userNumber2 = Number(document.getElementById("num-2").value);
+    const userNumber3 = Number(document.getElementById("num-3").value);
+    const userNumbers = [userNumber1, userNumber2, userNumber3];
 
-console.log(userNumber1, userNumber2, userNumber3);
+    if (userNumber1 === userNumber2 && userNumber1 === userNumber3) {
+        document.getElementById("numeros-ordenados").innerHTML = `Los números son iguales: ${userNumbers.join(", ")}`;
+    } else{
+        const compareNumbers = (a, b) => b - a;
 
-if ( userNumber1 === userNumber2 && userNumber1 === userNumber3){
-    console.log(`Los números son iguales:`)
+    userNumbers.sort((b, a) => a - b);
+
+    const text = `Los números ordenados de mayor a menor son: ${userNumbers.join(", ")}`
+    document.getElementById("numeros-ordenados").innerHTML = text;
+    }
+
 }
 
-const compareNumbers = (a, b) => b - a;
 
-userNumbers.sort(function (b, a){
-    return a - b;
-})
 
-console.log(userNumbers.join(", "));
-   
